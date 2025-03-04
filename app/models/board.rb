79619e481ee38c315bd5e5d4e9541d0cad77a28b
Amy May Nguyen <anguye10@uchicago.edu>
2025-03-04 21:10:33 +0000

    Looks good but not functional yet

---
+++ app/models/board.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Board < ApplicationRecord
+  validates(:name, {
+    :uniqueness => { :message => "has already been taken" },
+  })
+  has_many(:posts, class_name: "Post", foreign_key: "board_id", primary_key: "id")
+end
